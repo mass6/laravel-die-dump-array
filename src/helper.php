fe0220aa08e65c865f0b11089a87bcfa626586a1
mass6 <samcharmy@gmail.com>
2022-05-26 21:16:59 +0200

    Add header warning error

--- src/helper.php
+++ src/helper.php
@@ -5,6 +5,10 @@ use Symfony\Component\VarDumper\VarDumper;
 if (!function_exists('dda')) {
     function dda(...$vars)
     {
+        if (!in_array(\PHP_SAPI, ['cli', 'phpdbg'], true) && !headers_sent()) {
+            header('HTTP/1.1 500 Internal Server Error');
+        }
+
         foreach ($vars as $var) {
             if (method_exists($var, 'toArray')) {
                 return Vardumper::dump($var->toArray());
@@ -19,6 +23,10 @@ if (!function_exists('dda')) {
 if (!function_exists('da')) {
     function da(...$vars)
     {
+        if (!in_array(\PHP_SAPI, ['cli', 'phpdbg'], true) && !headers_sent()) {
+            header('HTTP/1.1 500 Internal Server Error');
+        }
+
         foreach ($vars as $var) {
             if (method_exists($var, 'toArray')) {
                 return VarDumper::dump($var->toArray());
